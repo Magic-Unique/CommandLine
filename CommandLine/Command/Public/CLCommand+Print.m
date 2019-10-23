95bc0dcabb36bc2040da8126a6a68bf67f967a66
冷秋 <516563564@qq.com>
2019-10-23 22:51:35 +0800

    Opt: help banner printing

--- CommandLine/Command/Public/CLCommand+Print.m
+++ CommandLine/Command/Public/CLCommand+Print.m
@@ -28,7 +28,9 @@
         }
     };
     
-    BOOL containsAbbr = NO;
+    BOOL commandContainsAbbr = NO;
+    BOOL requireContainsAbbr = NO;
+    BOOL optionalContainsAbbr = NO;
     
     CLSortType parametersSortType = [CLCommand parametersSortType];
     
@@ -36,19 +38,22 @@
     NSMutableArray<CLQuery *>   *optionalQueries    = [NSMutableArray array];
     NSMutableArray<CLQuery *>   *requireQueries     = [NSMutableArray array];
     for (CLQuery *query in self.queries.allValues) {
-        if (query.abbr != 0) {
-            containsAbbr = YES;
-        }
         if (query.isOptional) {
+            if (query.abbr != 0) {
+                optionalContainsAbbr = YES;
+            }
             [optionalQueries addObject:query];
         } else {
+            if (query.abbr != 0) {
+                requireContainsAbbr = YES;
+            }
             [requireQueries addObject:query];
         }
     }
     NSMutableArray<CLFlag *>    *flags              = [NSMutableArray array];
     for (CLFlag *flag in self.customFlags.allValues) {
         if (flag.abbr != 0) {
-            containsAbbr = YES;
+            optionalContainsAbbr = YES;
         }
         [flags addObject:flag];
     }
@@ -80,20 +85,26 @@
     }
     
     //  Calcular max key length
-    [self.subcommands enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, CLCommand * _Nonnull obj, BOOL * _Nonnull stop) {
-        CLCompareMaxLength([obj titleWithAbbr:containsAbbr]);
+    [subcommands enumerateObjectsUsingBlock:^(CLCommand * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:commandContainsAbbr]);
     }];
-    [self.queries enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, CLQuery * _Nonnull obj, BOOL * _Nonnull stop) {
-        CLCompareMaxLength([obj titleWithAbbr:containsAbbr]);
+    [requireQueries enumerateObjectsUsingBlock:^(CLQuery * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:requireContainsAbbr]);
     }];
-    [self.flags enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, CLFlag * _Nonnull obj, BOOL * _Nonnull stop) {
-        CLCompareMaxLength([obj titleWithAbbr:containsAbbr]);
+    [requireIOPaths enumerateObjectsUsingBlock:^(CLIOPath * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:requireContainsAbbr]);
     }];
-    [self.IOPaths enumerateObjectsUsingBlock:^(CLIOPath * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        CLCompareMaxLength([obj titleWithAbbr:containsAbbr]);
+    [optionalQueries enumerateObjectsUsingBlock:^(CLQuery * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:optionalContainsAbbr]);
+    }];
+    [flags enumerateObjectsUsingBlock:^(CLFlag * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:optionalContainsAbbr]);
+    }];
+    [optionalIOPaths enumerateObjectsUsingBlock:^(CLIOPath * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        CLCompareMaxLength([obj titleWithAbbr:optionalContainsAbbr]);
     }];
     [self.predefineFlags enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, CLFlag * _Nonnull obj, BOOL * _Nonnull stop) {
-        CLCompareMaxLength([obj titleWithAbbr:containsAbbr]);
+        CLCompareMaxLength([obj titleWithAbbr:optionalContainsAbbr]);
     }];
     
     // Usage
@@ -106,16 +117,17 @@
         if (self.subcommands.count && self.task) {
             CCPrintf(style, @"%@ [%@]", commands, CLCurrentLanguage.helpCommand);
         } else if (self.task) {
-            NSString *suffix = @"";
+            CCPrintf(style, @"%@", commands);
             if (self.IOPaths) {
-                NSArray *requireList = [requireIOPaths valueForKeyPath:@"title"];
-                NSArray *optionalList = [optionalIOPaths valueForKeyPath:@"title"];
-                NSMutableArray *paths = [NSMutableArray array];
-                [paths addObjectsFromArray:requireList];
-                [paths addObjectsFromArray:optionalList];
-                suffix = [paths componentsJoinedByString:@" "];
+                if (requireIOPaths.count) {
+                    NSArray *requireList = [requireIOPaths valueForKeyPath:@"title"];
+                    CCPrintf(CCStyleForegroundColorPurple, @" %@", [requireList componentsJoinedByString:@" "]);
+                }
+                if (optionalIOPaths.count) {
+                    NSArray *optionalList = [optionalIOPaths valueForKeyPath:@"title"];
+                    CCPrintf(CCStyleForegroundColorYellow, @" %@", [optionalList componentsJoinedByString:@" "]);
+                }
             }
-            CCPrintf(style, @"%@ %@", commands, suffix);
         } else if (self.subcommands.count) {
             CCPrintf(style, @"%@ <%@>", commands, CLCurrentLanguage.helpCommand);
         } else {
@@ -135,7 +147,7 @@
         CCPrintf(0, @"\n");
         [subcommands enumerateObjectsUsingBlock:^(CLCommand * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             CLCommand *command = obj;
-            NSString *title = [command titleWithAbbr:containsAbbr];
+            NSString *title = [command titleWithAbbr:commandContainsAbbr];
             
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorGreen, title);
@@ -156,7 +168,7 @@
         CCPrintf(0, @"\n");
         
         [requireQueries enumerateObjectsUsingBlock:^(CLQuery * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:requireContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorGreen, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
@@ -169,7 +181,7 @@
         }
         
         [requireIOPaths enumerateObjectsUsingBlock:^(CLIOPath * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:requireContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorGreen, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
@@ -189,7 +201,7 @@
         CCPrintf(0, @"\n");
         
         [optionalQueries enumerateObjectsUsingBlock:^(CLQuery * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:optionalContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorBlue, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
@@ -203,7 +215,7 @@
         }
         
         [flags enumerateObjectsUsingBlock:^(CLFlag * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:optionalContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorBlue, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
@@ -217,7 +229,7 @@
         }
         
         [optionalIOPaths enumerateObjectsUsingBlock:^(CLIOPath * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:optionalContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorBlue, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
@@ -231,7 +243,7 @@
         }
         
         [self.predefineFlags.allValues enumerateObjectsUsingBlock:^(CLFlag * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSString *title = [obj titleWithAbbr:containsAbbr];
+            NSString *title = [obj titleWithAbbr:optionalContainsAbbr];
             CCPrintf(0, @"    ");
             CCPrintf(CCStyleForegroundColorBlue, title);
             CCPrintf(0, CLSpaceString(maxKey + 3 - strlen(title.UTF8String)));
